3a3e5086781aeb4a3df195d4746ef7bdb6c0ca38
ceoca-ovidiu-30126 <Ceoca.Al.Ovidiu@utcluj.didatec.ro>
2021-05-14 11:50:46 +0300

    Reformat code for exercise 1 2 3 4.

--- src/main/java/aut/utcluj/isp/ex2/OnlineShop.java
+++ src/main/java/aut/utcluj/isp/ex2/OnlineShop.java
@@ -4,20 +4,20 @@ package aut.utcluj.isp.ex2;
  * @author stefan
  */
 public class OnlineShop extends Shop {
+
     private String webAddress;
 
     public OnlineShop(String name, String city, String webAddress) {
-       super(name,city);
-       this.webAddress=webAddress;
+        super(name, city);
+        this.webAddress = webAddress;
     }
 
     public String getWebAddress() {
         return webAddress;
     }
 
-	@Override
-	public String toString() {
-		return super.toString()+" Web address: " + webAddress;
-	}
-    
+    @Override
+    public String toString() {
+        return super.toString() + " Web address: " + webAddress;
+    }
 }
